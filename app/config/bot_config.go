b523e1ee33cdb4a8f9dd2d77d9f7cd142280e0de
noobclear <newbclear@gmail.com>
2017-09-04 03:22:04 -0700

    Set up lifx connection

--- app/config/bot_config.go
+++ app/config/bot_config.go
@@ -6,4 +6,5 @@ type BotConfig struct {
 	TargetChannel    string `json:"target_channel"`
 	RateLimit        int    `json:"rate_limit"`
 	WitAccessToken   string `json:"wit_access_token"`
+	LIFXAccessToken  string `json:"lifx_access_token"`
 }
